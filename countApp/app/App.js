40e47687715297be2c0916b761b64616b755201a
guinegrijo <guilhermenegrijo@gmail.com>
2025-02-05 16:19:57 -0300

    React Native

---
+++ countApp/app/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App () {
+    const [count, setCount] = useState(0)
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Clique para contar</Text>
+            <Text style={styles.text}>{count}</Text>
+            <Button
+                title="Clique"
+                onPress={()=>{setCount(count+1)}}
+                
+            />
+            <Button
+                title="Resete"
+                onPress={()=>{setCount(0)}}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#964b00',
+        alignItems: 'center',
+        justifyContent: "center"
+    },
+
+    text: {
+        fontSize: 50
+    }
+})
